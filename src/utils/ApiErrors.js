5ba0be18f36fdd368806ce0b7dd7c9835a3fe9b4
Jameel3654 <101401998+Jameel3654@users.noreply.github.com>
2025-10-24 11:25:27 +0500

    all setup and configure

--- src/utils/ApiErrors.js
+++ src/utils/ApiErrors.js
@@ -3,7 +3,7 @@ class ApiEroors extends Error {
     statusCode,
     message = "Something went wrong",
     errors = [],
-    statck = ""
+    stack = ""
   ) {
     super(message);
     this.statusCode = statusCode;
--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,6 +1,6 @@
 // 2nd method
-const asyncHandler = (fn) => (req, res, next) => {
-  Promise.resolve(fn(req, res, next)).catch((error) => {
+const asyncHandler = (reqHandler) => (req, res, next) => {
+  Promise.resolve(reqHandler(req, res, next)).catch((error) => {
     next(error);
   });
 };
